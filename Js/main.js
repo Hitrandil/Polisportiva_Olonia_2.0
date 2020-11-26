9f4a24afe4b5d46092d8b901fb754b66b6242eac
Hitrandil <hitrandil@Hitrandil.localdomain>
2020-11-26 12:54:22 +0100

    creazione navbar, aggiunta primi articoli

---
+++ Js/main.js
@@ -0,0 +1,7 @@
+function navBorderOn(id) {
+    document.getElementById(id).style = " color: #008DD2; border-bottom: solid 3px green";
+}
+
+function navBorderOff(id) {
+    document.getElementById(id).style.color = "#262626";
+}
